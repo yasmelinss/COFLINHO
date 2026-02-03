4acee64d216397d1e90529a60e0ae8cce2df1377
Yasmin Messias <yasmin.institucional@usp.br>
2026-02-02 23:23:13 -0300

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { getCurrentWindow } from "@tauri-apps/api/window";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-
+import Home from "./pages/Home";
+import Historico from "./pages/Historico";
+import Metas from "./pages/Metas";
 
 const appWindow = getCurrentWindow();
 
@@ -10,61 +13,75 @@ function App() {
   const [saldo, setSaldo] = useState<number>(0);
 
   return (
-    <div className="app-container">
-      {/* 1. BARRA DE TÍTULO CUSTOMIZADA */}
-      <nav data-tauri-drag-region className="titlebar">
-        <div className="titlebar-label">Coflinho 🐷</div>
-        <div className="titlebar-actions">
-          <button className="titlebar-button" onClick={() => appWindow.minimize()}>
-            ─
-          </button>
-          <button className="titlebar-button" onClick={() => appWindow.toggleMaximize()}>
-            ▢
-          </button>
-          <button className="titlebar-button exit" onClick={() => appWindow.close()}>
-            ✕
-          </button>
-        </div>
-      </nav>
-
-      {/* 2. CONTEÚDO DO APLICATIVO */}
-
-
-      <main className="content">
-        <div className="balanço-patrimonial">
-          <h1>Salvo no mês</h1>
-          <h2>R$ X</h2>
-          <p> Você está a X% de alcançar sua meta</p>
-          <img src="src-tauri/icons/128x128@2x.png" alt="Piggy bank" />
-        </div>
-
-        <div className="ações">
-          <button onClick={() => +1}>
-            Novo Ganho
-          </button>
-          <button>
-            Novo Gasto
-          </button>
-        </div>
-
-
-
-
-        <div className="balance-card">
-          <p>Saldo total</p>
-          <h1>R$ {saldo.toFixed(2)}</h1>""
-          
-          <div className="actions">
-            <button onClick={() => setSaldo(saldo + 10)}>
-              Depositar R$ 10
+    <BrowserRouter>
+      <div className="app-container">
+        {/* 1. BARRA DE TÍTULO CUSTOMIZADA */}
+        <div data-tauri-drag-region className="titlebar">
+          <div className="titlebar-label">Coflinho 🐷</div>
+          <div className="titlebar-actions">
+            <button className="titlebar-button" onClick={() => appWindow.minimize()}>
+              ─
             </button>
-            <button onClick={() => setSaldo(0)} className="reset-btn">
-              Esvaziar Cofre
+            <button className="titlebar-button" onClick={() => appWindow.toggleMaximize()}>
+              ▢
+            </button>
+            <button className="titlebar-button exit" onClick={() => appWindow.close()}>
+              ✕
             </button>
           </div>
         </div>
-      </main>
-    </div>
+
+
+        {/* 2. NAVEGAÇÃO */}
+        <nav className="navigation">
+          <Link to="/">Home</Link>
+          <Link to="/historico">Histórico</Link>
+          <Link to="/metas">Metas</Link>
+        </nav>
+
+        {/* 2. CONTEÚDO DO APLICATIVO */}
+
+
+        <main className="content">
+          <div className="balanço-patrimonial">
+            <h1>Salvo no mês</h1>
+            <h2>R$ X</h2>
+            <p> Você está a X% de alcançar sua meta</p>
+            <img src="src-tauri/icons/128x128@2x.png" alt="Piggy bank" />
+          </div>
+
+          <div className="ações">
+            <button onClick={() => +1}>
+              Novo Ganho
+            </button>
+            <button>
+              Novo Gasto
+            </button>
+          </div>
+
+          {/* <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/historico" element={<Historico />} />
+            <Route path="/metas" element={<Metas />} />
+          </Routes> */}
+
+
+          <div className="balance-card">
+            <p>Saldo total</p>
+            <h1>R$ {saldo.toFixed(2)}</h1>""
+
+            <div className="actions">
+              <button onClick={() => setSaldo(saldo + 10)}>
+                Depositar R$ 10
+              </button>
+              <button onClick={() => setSaldo(0)} className="reset-btn">
+                Esvaziar Cofre
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
